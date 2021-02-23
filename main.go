1e0b7101352cf2b63eedafbbe8dac3991411d32a
tamama9527 <deus85072@mail.fcu.edu.tw>
2021-02-23 11:04:51 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "strings"
+	"bytes"
+	"encoding/json"
+	"fmt"
+    "os"
+	"net/http"
+	"regexp"
+	"github.com/tamama9527/util"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+// You more than likely want your "Bot User OAuth Access Token" which starts with "xoxb-"
+var api = slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+func main() {
+	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		body := buf.String()
+		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: os.Getenv("SLACK_EVENT_TOKEN")}))
+		if e != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+            case *slackevents.MessageEvent:
+				fmt.Println(ev.Text)
+				str := []byte(ev.Text)
+				reg := regexp.MustCompile(`\*<(\S*)\|(.*)>\*`)
+				result := reg.FindSubmatch(str)
+				url := string(result[1])
+				title := string(result[2])
+                fmt.Println("--------------------")
+                if len(ev.Attachments) > 0{
+                    for i:= 0; i < len(ev.Attachments); i++{
+                        fmt.Println(ev.Attachments[i].Text)
+                        if strings.Contains(ev.Attachments[i].Text,"*Finish*: No → Yes"){
+							util.NotionExport(1,2)
+							fmt.Println("Got it")
+                        }
+                    }
+                } else {
+                    fmt.Println(ev.Text)
+                }
+			}
+		}
+	})
+	fmt.Println("[INFO] Server listening")
+	fmt.Println(os.Getenv("SLACK_EVENT_TOKEN"))
+	fmt.Println(os.Getenv("SLACK_BOT_TOKEN"))
+	http.ListenAndServe(":3000", nil)
+}
